e2da6f8f67abe659a08041ecaacffba6a5a73056
Panchenko Daria <pancenkodara181@gmail.com>
2025-06-05 17:30:48 +0700

    Delete StackCalculator directory

--- StackCalculator/src/main/java/calculator/Command.java
+++
@@ -1,7 +0,0 @@
-package calculator;
-
-import calculator.exceptions.CommandException;
-
-public abstract class Command {
-    public abstract void execute(Context context, String[] args) throws CommandException;
-}
